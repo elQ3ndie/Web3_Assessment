733f6dde43b8a1aa744b81358eaf12b4c2b56e36
elcarim <andrewmiracle300@gmail.com>
2023-12-17 17:05:10 +0100

    Initial commit

---
+++ src/questions.js
@@ -0,0 +1,17 @@
+// questions.js
+const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "Berlin", "London", "Madrid"],
+      correctAnswer: "Paris",
+    },
+    {
+      question: "Which programming language is this quiz written in?",
+      options: ["JavaScript", "Python", "Java", "C++"],
+      correctAnswer: "JavaScript",
+    },
+    
+  ];
+  
+  module.exports = questions;
+  
